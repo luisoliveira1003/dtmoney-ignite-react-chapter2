9175c7c1fe665f7d5bddb3bc616c58068334a5eb
Luis Andre <luis.oliveira10031992@gmail.com>
2021-03-24 18:39:11 -0300

    the 'Conext API' in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import Modal from "react-modal";
-import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
+
+import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
 
@@ -21,7 +23,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
