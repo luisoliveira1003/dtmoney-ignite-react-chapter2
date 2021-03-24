dd7e0bb52af51062196afe506797860ddaba22d1
Luis Andre <luis.oliveira10031992@gmail.com>
2021-03-23 23:04:08 -0300

    refactoring 'TransactionModal' to new component 'NewTransactionModal'

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Header } from "./components/Header";
 import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
@@ -25,12 +26,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
